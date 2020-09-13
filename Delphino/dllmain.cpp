5e50fa003d4ec837e836e8451ce4cf640c6d5952
DisabledMallis <mallisiscool@gmail.com>
2020-09-13 17:13:46 -0400

    Draw line stuff expiremnet

--- Delphino/dllmain.cpp
+++ Delphino/dllmain.cpp
@@ -5,14 +5,33 @@
 #include <iostream>
 #include <jni.h>
 #include <gl/GL.h>
+#include <wingdi.h>
+#include <MinHook.h>
+
+using namespace std;
 
 typedef jint(*hJNI_GetCreatedJavaVMs)(JavaVM** vmBuf, jsize bufLen, jsize* nVMs);
 hJNI_GetCreatedJavaVMs oJNI_GetCreatedJavaVMs;
 
+typedef BOOL (*type_wglSwapLayerBuffers)(HDC, UINT);
+type_wglSwapLayerBuffers owglSwapLayerBuffers;
+
+BOOL oglHook(HDC hdc, UINT uint) {
+    cout << "Render!" << endl;
+
+    glBegin(GL_LINES);
+    glLineWidth(4);
+    glVertex2f(0, 0);
+    glVertex2f(100, 100);
+    glEnd();
+
+    //Sleep(100);
+
+    return owglSwapLayerBuffers(hdc, uint);
+}
+
 JavaVM* jvm;
 JNIEnv* jenv;
-
-using namespace std;
 
 int __stdcall StartDelphino() {
     MessageBoxA(nullptr, "Delphino has injected!", "DELPHINO", MB_OK);
@@ -33,10 +52,27 @@ int __stdcall StartDelphino() {
 
     if (jenv != nullptr) {
         cout << "Found JVM & JEnv!" << endl;
+        HMODULE oGlHandle = GetModuleHandleA("OPENGL32.dll");
+        FARPROC fp = GetProcAddress(oGlHandle, "wglSwapBuffers");
 
-
-
-
+        if (MH_Initialize() == MH_OK) {
+            cout << "Initialized MinHook" << endl;
+            if (MH_CreateHook(fp, &oglHook, (LPVOID*)&owglSwapLayerBuffers) == MH_OK) {
+                cout << "Created opengl hook!" << endl;
+                if (MH_EnableHook(fp) == MH_OK) {
+                    cout << "Enabled opengl hook!" << endl;
+                }
+                else {
+                    cout << "Failed to enable opengl hook!" << endl;
+                }
+            }
+            else {
+                cout << "Failed to create opengl hook!" << endl;
+            }
+        }
+        else {
+            cout << "Failed to initialized MinHook!" << endl;
+        }
     }
     else {
         cout << "Failed to find JVM & JEnv!" << endl;
